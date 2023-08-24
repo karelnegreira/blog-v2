47b16131837db04fd68be2ce1e186df8901d6899
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-08-24 20:41:29 +1200

    Featured post component is set up and running

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,16 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import {Categories, PostCard, PostWidget } from '../components';
 import { getPosts } from '../services';
+import  { FeaturedPosts } from '../sections';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-
-
 export default function Home({ posts }) {
-
-  
 
   return (
     <div className="container mx-auto px-10 mb-8">
-      FeaturedPosts 
+      <FeaturedPosts /> 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
