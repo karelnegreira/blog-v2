7979d31181ab9e23c36d1d544ed6bc199e0e5ba5
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-08-25 22:38:55 +1200

    some comment updates

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -13,13 +13,13 @@ export default async function comments(req, res) {
     headers: {
       authorization: `Bearer ${token}`
     }
-  })
+  });
 
-  const query = gql `
+  const query = gql`
         mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
             createComment(data: { name: $name, email: $email, comment: $comment, post: { connect: {slug: $slug } } }) { id }
         }
-  `
+  `;
   
   try {
     const result = await graphQlClient.request(query, req.body);
